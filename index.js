df90ceb065cff4997712efff81577636eff45c7f
Jason Vance <jbvance@gmail.com>
2017-12-21 03:07:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ var destructivelyRemoveLastKitten = () => kittens.pop();
 
 var destructivelyRemoveFirstKitten = () => kittens.shift();
 
-var appendKitten = (name) => [...kittens, name]
+var appendKitten = name => [...kittens, name];
