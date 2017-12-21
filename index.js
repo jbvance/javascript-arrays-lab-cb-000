33f53837324a25e3f82b7e34fc4b2b440a417770
Jason Vance <jbvance@gmail.com>
2017-12-21 03:13:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,5 @@ var appendKitten = name => [...kittens, name];
 var prependKitten = name => [name, ...kittens];
 
 var removeLastKitten = () => kittens.slice(0, kittens.length - 1);
+
+var removeFirstKitten() => kittens.slice(1);
